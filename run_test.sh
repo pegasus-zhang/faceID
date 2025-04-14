bb7d192e437dc7eaec98ca1dcf78dc707cdfdde9
zzk <pegasus-zhang@163.com>
2025-04-14 15:02:52 +0800

    Fix:1.修改运行脚本bug

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,4 @@
 SCRIPT_DIR=$(dirname "$0")
 source $SCRIPT_DIR/ros1_interface/devel/setup.bash
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
 taskset -c 5,6 ./workspace/face_id_app --host_name Zhangzhaokang --print_log --show
